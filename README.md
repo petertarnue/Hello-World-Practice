ca164be992001c493523a2b4052d3d66bb102b90
Peter K. Tarnue <petertarnue@gmail.com>
2021-06-20 15:48:23 +0000

    This is a simple python timestable code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hello-World-Practice
 This is just the beginning of my study and pratices of career goal of becoming software developer
 Hello Everyone my name is Peter K. Tarnue. I am currently a partime employee at Youth Crime Watch of Liberia.
+
+This is just the beginning of the journing.
---
+++ python.py
@@ -0,0 +1,21 @@
+def timetable(x, y):
+    x = int(input)
+    y = int(input)
+    for x in range(50):
+        product = x * y
+        result = product
+    return result
+
+def timetable(x, y):
+    if x < y:
+        return x
+    return y
+
+def love(name):
+    first_name = input("Enter your first name: ")
+    middle_name = input("Enger your middle name: ")
+    last_name = input("Enter your last name: ")
+    name = first_name +" "+ middle_name + " "+ last_name
+    return name
+
+
